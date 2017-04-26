9ff32893de6b199aa4e846cacf12d41139ff625f
kakaguo <guojunru1989@126.com>
2017-04-26 16:56:56 +0800

    add showDetail

--- src/views/root.vue
+++ src/views/root.vue
@@ -6,48 +6,68 @@
     <div class="main-table" v-for="building in buildings">
       <h3 class="name">{{ building.name }}</h3>
       <Row class="table">
-        <Col span="6">计划开始<p>{{ building.start_date | dateFormat}}</p></Col>
-        <Col span="6">计划完成<p>{{ building.end_date | dateFormat}}</p></Col>
-        <Col span="6">计划工期<p>{{ building.total_duration }}天</p></Col>
-        <Col span="6">已经施工<p>{{ building.total_elapsed }}天</p></Col>
+        <Col span="6">
+        计划开始<p>{{ building.start_date | dateFormat}}</p></Col>
+        <Col span="6">
+        计划完成<p>{{ building.end_date | dateFormat}}</p></Col>
+        <Col span="6">
+        计划工期<p>{{ building.total_duration }}天</p></Col>
+        <Col span="6">
+        已经施工<p>{{ building.total_elapsed }}天</p></Col>
       </Row>
       <ul>
         <li>计划进度: {{ building.plan_progress }}</li>
         <li>实际进度: {{ building.real_progress }}</li>
       </ul>
-
-      <h4><i class="iconfont icon-similarproduct"></i> 分布工程</h4>
-      <ul>
-        <li v-for="part in building.parts">
-          <Row class="table">
-            <Col span="6">计划开始<p>{{ part.start_date | dateFormat}}</p></Col>
-            <Col span="6">计划完成<p>{{ part.end_date | dateFormat}}</p></Col>
-            <Col span="6">计划工期<p>{{ part.total_duration }}天</p></Col>
-            <Col span="6">已经施工<p>{{ part.total_elapsed }}天</p></Col>
-          </Row>
-          <ul>
-            <li>下面两个做成进度条</li>
-            <li>计划进度: {{ part.plan_progress }}</li>
-            <li>实际进度: {{ part.real_progress }}</li>
-          </ul>
-        </li>
-      </ul>
+      <div class="part">
+        <h4 class="list"><i class="iconfont icon-similarproduct"></i> 分布工程</h4>
+        <span class="part-icon part-unfold"><i class="iconfont icon-moreunfold" @click="showDetail"></i></span>
+        <span class="part-icon part-fold"><i class="iconfont icon-less" @click="hideDetail"></i></span>
+        <ul v-show="detailShow">
+          <li v-for="part in building.parts">
+            <Row class="table">
+              <Col span="6">
+              计划开始<p>{{ part.start_date | dateFormat}}</p></Col>
+              <Col span="6">
+              计划完成<p>{{ part.end_date | dateFormat}}</p></Col>
+              <Col span="6">
+              计划工期<p>{{ part.total_duration }}天</p></Col>
+              <Col span="6">
+              已经施工<p>{{ part.total_elapsed }}天</p></Col>
+            </Row>
+            <ul>
+              <li>下面两个做成进度条</li>
+              <li>计划进度: {{ part.plan_progress }}</li>
+              <li>实际进度: {{ part.real_progress }}</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
       <hr>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  props: ['db'],
-  data () {
-    let buildings = this.db.buildings
+  export default {
+    props: ['db'],
+    data () {
+      let buildings = this.db.buildings
+      return {
+        buildings: buildings,
+        detailShow: false
+      }
+    },
+    methods: {
+      showDetail () {
+        this.detailShow = true
+      },
+      hideDetail () {
+        this.detailShow = false
+      }
 
-    return {
-      buildings: buildings
     }
   }
-}
 </script>
 
 <style>
@@ -58,8 +78,17 @@ export default {
     margin: 25px;
   }
 
+  .main .iconfont {
+    font-weight: 700;
+  }
+
   .title {
     font-size: 28px;
+  }
+
+  .list {
+    font-size: 20px;
+    margin: 15px 0;
   }
 
   .search input {
@@ -78,11 +107,22 @@ export default {
     font-weight: 700;
     padding: 10px 0;
   }
+
   .main-table .table {
     text-align: center;
     font-size: 14px;
   }
+
   .main-table .table p {
     font-weight: 700;
   }
+
+  .part-icon {
+    display: inline-block;
+    width: 100%;
+    text-align: center;
+    font-size: 18px;
+    padding: 10px 0;
+  }
+
 </style>
