6f28ebeb78eec9f6587802f417f79e9567a6ea74
kakaguo <guojunru1989@126.com>
2017-04-26 16:12:28 +0800

    添加input组件

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
 import axios from 'axios'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+
+Vue.use(iView)
 
 Vue.config.productionTip = false
 
