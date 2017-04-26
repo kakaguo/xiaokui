bb377044d3df97bb017c397fd923e90723fd7c3f
kakaguo <guojunru1989@126.com>
2017-04-26 21:02:06 +0800

    增加动画

--- src/views/root.vue
+++ src/views/root.vue
@@ -20,31 +20,34 @@
       </ul>
       <div class="part">
         <h4 class="part-title"><i class="iconfont icon-similarproduct"></i> 分布工程</h4>
-        <span class="part-icon part-unfold" v-show="!detailShow"><i class="iconfont icon-moreunfold" @click="showDetail"></i></span>
-        <span class="part-icon part-fold" v-show="detailShow"><i class="iconfont icon-less" @click="hideDetail"></i></span>
-        <ul v-show="detailShow">
-          <li v-for="part in building.parts">
-            <Row class="table">
-              <Col span="6">
-              计划开始<p>{{ part.start_date | dateFormat}}</p></Col>
-              <Col span="6">
-              计划完成<p>{{ part.end_date | dateFormat}}</p></Col>
-              <Col span="6">
-              计划工期<p>{{ part.total_duration }}天</p></Col>
-              <Col span="6">
-              已经施工<p>{{ part.total_elapsed }}天</p></Col>
-            </Row>
-            <ul>
-              <progressbar :real="part.real_progress" :plan="part.plan_progress"></progressbar>
-            </ul>
-          </li>
-        </ul>
+        <span class="part-icon part-unfold" v-if="!building.detailShow" @click="showDetail(building)"><i class="iconfont icon-moreunfold"></i></span>
+        <span class="part-icon part-fold" v-if="building.detailShow" @click="hideDetail(building)"><i class="iconfont icon-less"></i></span>
+        <transition name="slide">
+          <ul v-if="building.detailShow">
+            <li v-for="part in building.parts">
+              <Row class="table">
+                <Col span="6">
+                计划开始<p>{{ part.start_date | dateFormat}}</p></Col>
+                <Col span="6">
+                计划完成<p>{{ part.end_date | dateFormat}}</p></Col>
+                <Col span="6">
+                计划工期<p>{{ part.total_duration }}天</p></Col>
+                <Col span="6">
+                已经施工<p>{{ part.total_elapsed }}天</p></Col>
+              </Row>
+              <ul>
+                <progressbar :real="part.real_progress" :plan="part.plan_progress"></progressbar>
+              </ul>
+            </li>
+          </ul>
+        </transition>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import Vue from 'Vue'
   import progressbar from './progressbar/progressbar.vue'
 
   export default {
@@ -57,12 +60,20 @@
       }
     },
     methods: {
-      showDetail () {
-        this.detailShow = true
+      showDetail (building) {
+        Vue.set(building, building.detailShow, true)
+        building.detailShow = true
       },
-      hideDetail () {
-        this.detailShow = false
+      hideDetail (building) {
+        Vue.set(building, building.detailShow, false)
+        building.detailShow = false
       }
+    },
+    created () {
+      for (let key in this.buildings) {
+        this.buildings[key].detailShow = false
+      }
+      console.log('1', this.buildings)
     },
     components: {
       progressbar
@@ -128,6 +139,17 @@
     text-align: center;
     font-size: 18px;
     padding: 10px 0;
+  }
+  .part .ivu-progress-bg {
+    height: 10px !important;
+  }
+
+  .slide-enter-active, .slide-leave-active {
+    transition: all .8s;
+  }
+  .slide-enter, .slide-leave-active {
+    transform: translateY(-50px);
+    opacity: 0;
   }
 
 </style>
